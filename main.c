e3f5329db3b03f31f729e3505c6fc729734e70cd
Ruslan Kuprieiev <kupruser@gmail.com>
2014-05-11 23:39:44 +0400

    add main and parse_cmd dummy

---
+++ main.c
@@ -0,0 +1,15 @@
+
+static int parse_cmd(char *s)
+{
+	puts(s);
+	return 0;
+}
+
+int main(int argc, char *argv[], char *envp[])
+{
+	while (1) {
+		print("user@host pwd $ )");
+		scanf("%s\n", %s);
+		parse_cmd(s);
+	}
+}
